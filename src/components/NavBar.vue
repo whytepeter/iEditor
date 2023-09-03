db451c52c88a91f71cfcb116e44db7dd3fbb34ee
whytepeter <emmanuelwhytepeter@gmail.com>
2023-09-03 20:31:28 +0100

    upate

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,22 @@
 <template>
   <div
-    class="fixed left-0 top-0 w-full p-2 px-6 px-md-10 bg-[#A670FF] flex items-center justify-between"
+    class="fixed pl-80 left-0 top-0 w-full p-2 px-6 px-md-10 bg-[#A670FF] flex items-center justify-between"
   >
-    <h2 class="text-white font-bold text-xl">iEDITOR</h2>
-
     <div class="flex items-center gap-4">
+      <h2 class="text-white font-bold text-xl ml-4">iEDITOR</h2>
+
       <input
-        class="focus:outline-none w-36 p-2 bg-[#8E5DDD] text-white focus:border-white focus:border"
+        class="focus:outline-none w-36 p-2 bg-[#8E5DDD] text-white focus:border-white focus:border capitalize"
         type="text"
         v-model="projectName"
       />
+    </div>
+    <div class="flex items-center gap-4">
+      <button
+        class="p-2 px-4 w-28 rounded bg-white text-[#150037] hover:opacity-95"
+      >
+        Preview
+      </button>
       <button
         class="p-2 px-4 w-28 rounded bg-white text-[#150037] hover:opacity-95"
       >
